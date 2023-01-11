a29a2d797449cb3b858c125d1b99dcaca4f3fdc8
Kuba Mrowiec <mruvekss@gmail.com>
2023-01-11 19:05:25 +0100

    fixed api problem

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,6 +8,8 @@ import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+
+
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
@@ -18,13 +20,14 @@ const ExerciseDetail = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
     const fetchExercisesData = async () => {
-      
+
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+      
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
-
+ 
       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
       setExerciseVideos(exerciseVideosData.contents);
 
@@ -36,7 +39,7 @@ const ExerciseDetail = () => {
     };
 
     fetchExercisesData();
-  }, [id]);
+  }, []);
 
   if (!exerciseDetail) return <div>No Data</div>;
 
