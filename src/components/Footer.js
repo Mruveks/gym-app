d93df0ec6b22a013132f82990fe5d0ff13a085f5
Kuba Mrowiec <mruvekss@gmail.com>
2023-01-16 19:16:03 +0100

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" fontStyle="italic" pb="40px">
+          Kuba Mrowiec
+        </Typography>
+      </Stack>
+    </Box>
+  )
 }
 
 export default Footer
